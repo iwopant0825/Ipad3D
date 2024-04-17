f4436d91c2d96d64d20cecb6353ef4cd0ee2755b
iwopant0825 <129718228+iwopant0825@users.noreply.github.com>
2024-04-17 12:41:58 +0900

    기본 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { Box, Environment, OrbitControls, useGLTF } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Model } from "./Model"
 import * as THREE from 'three'
+import { Analytics } from "@vercel/analytics/react"
+import { Suspense } from "react"
+import Loading from "./Loding"
 
 function App() {
   return (
     <>
-      <Canvas gl={{antialias:true}}>
-        <Environment preset="city" />
-        <pointLight position={[10, 10, 10]} intensity={1.5} />
-        <OrbitControls enablePan={false} enableZoom={false}/>
-        <Model scale={4.5}/>
-      </Canvas>
+      <Analytics/>
+      <Suspense fallback={<Loading/>}>
+        <Canvas gl={{antialias:true}}>
+          <Environment preset="city" />
+          <pointLight position={[10, 10, 10]} intensity={1.5} />
+          <OrbitControls enablePan={false} enableZoom={false}/>
+          <Model scale={4.5}/>
+        </Canvas>        
+      </Suspense>
+
     </>
   )
 }
---
+++ src/Loding.jsx
@@ -0,0 +1,17 @@
+import { PuffLoader } from "react-spinners";
+
+const override = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  backgroundColor: "#202020"
+};
+
+export default function Loading() {
+  return (
+    <div style={override}>
+      <PuffLoader color="#36d7b7" />
+    </div>
+  );
+}
